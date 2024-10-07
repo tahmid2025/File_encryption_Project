8008279dce0181123fe447aa78e8c1bd767741c3
Mohammed-Tahmid-Uddin <mohammedtahmiduddin3@gmail.com>
2024-10-07 19:18:26 +0100

    Encryption key generator

--- main.py
+++ main.py
@@ -5,7 +5,9 @@ import os
 import random
 import string
 
-
+# This is a function to generate my Encryption key
+def encryption_key(length=6):
+    return "".join(random.choices(string.ascii_letters + string.digits, k=length))
 
 
 
